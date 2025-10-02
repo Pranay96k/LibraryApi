0a88e7521f14998b153a4f38311f3ffca980de48
Pranay96k <130050745+Pranay96k@users.noreply.github.com>
2025-10-02 16:36:16 +0530

    Update SP GetOverduebooks.sql

--- SP GetOverduebooks.sql
+++ SP GetOverduebooks.sql
@@ -3,16 +3,8 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT
-        br.BorrowId,
-        br.MemberId,
-        m.Name AS MemberName,
-        m.Email,
-        br.BookId,
-        b.Title AS BookTitle,
-        br.BorrowDate,
-        DATEADD(day,14,br.BorrowDate) AS DueDate,
-        DATEDIFF(day, DATEADD(day,14,br.BorrowDate), GETDATE()) AS DaysOverdue
+    SELECT br.BorrowId, br.MemberId, m.Name AS MemberName, m.Email, br.BookId, b.Title AS BookTitle, br.BorrowDate,
+DATEADD(day,14,br.BorrowDate) AS DueDate, DATEDIFF(day, DATEADD(day,14,br.BorrowDate), GETDATE()) AS DaysOverdue
     FROM BorrowRecords br
     JOIN Members m ON br.MemberId = m.MemberId
     JOIN Books b ON br.BookId = b.BookId
@@ -21,3 +13,4 @@ BEGIN
     ORDER BY DueDate ASC;
 END;
 GO
+
