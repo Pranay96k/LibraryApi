a916125c96d6be4059e85a13d6974e49b5654f65
Pranay96k <130050745+Pranay96k@users.noreply.github.com>
2025-10-02 16:33:52 +0530

    Update SP GetOverduebooks.sql

--- SP GetOverduebooks.sql
+++ SP GetOverduebooks.sql
@@ -1,29 +1,23 @@
 CREATE PROCEDURE sp_GetOverdueBooks
-  @MarkReturned BIT = 0
 AS
 BEGIN
-  SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-  IF @MarkReturned = 0
-  BEGIN
-    SELECT br.BorrowId, br.MemberId, m.Name AS MemberName, m.Email, br.BookId, b.Title AS BookTitle, br.BorrowDate,
-DATEADD(DAY, 14, br.BorrowDate) AS DueDate, DATEDIFF(DAY, DATEADD(DAY, 14, br.BorrowDate), GETDATE()) AS DaysOverdue
+    SELECT
+        br.BorrowId,
+        br.MemberId,
+        m.Name AS MemberName,
+        m.Email,
+        br.BookId,
+        b.Title AS BookTitle,
+        br.BorrowDate,
+        DATEADD(day,14,br.BorrowDate) AS DueDate,
+        DATEDIFF(day, DATEADD(day,14,br.BorrowDate), GETDATE()) AS DaysOverdue
     FROM BorrowRecords br
     JOIN Members m ON br.MemberId = m.MemberId
     JOIN Books b ON br.BookId = b.BookId
     WHERE br.IsReturned = 0
-      AND DATEADD(DAY, 14, br.BorrowDate) < GETDATE()
-    ORDER BY DaysOverdue DESC;
-  END
-  ELSE
-  BEGIN
-    UPDATE br
-    SET br.IsReturned = 1, br.ReturnDate = GETDATE()
-    FROM BorrowRecords br
-    WHERE br.IsReturned = 0
-      AND DATEADD(DAY, 14, br.BorrowDate) < GETDATE();
-
-    SELECT @@ROWCOUNT AS RowsUpdated;
-  END
-END
+      AND DATEADD(day, 14, br.BorrowDate) < GETDATE()
+    ORDER BY DueDate ASC;
+END;
 GO
